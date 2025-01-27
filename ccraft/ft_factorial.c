dc4cb4568e67a8fb2136d420fc13941aefaa215b
Hamza Benmohamd <hbenmoha@e2r2p11.1337.ma>
2025-01-27 20:28:40 +0100

    add new exercise

--- ccraft/ft_factorial.c
+++ ccraft/ft_factorial.c
@@ -1,4 +1,29 @@
+
+// ft_factorial while:
+/*
 int ft_factorial(int nb)
 {
-    
+    int x = nb;
+    if (nb < 0)
+        return (0);
+    if (nb == 0)
+        return (1);
+    while (--x)
+        nb *= x;
+    return (nb);
 }
+*/
+
+// ft_factorial recursion:
+int ft_factorial(int nb)
+{
+    if(nb <= 1)
+        return (nb >= 0);
+    return (nb*ft_factorial(nb-1));
+}
+
+// #include <stdio.h>
+// int main (void)
+// {
+//     printf("%d\n",ft_factorial(0));
+// }
